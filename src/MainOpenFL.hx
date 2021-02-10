2874f782b2c5cb89898b24bcc3d640d09d8d4a96
chaotive <leonardomontenegro123@gmail.com>
2021-02-10 20:45:40 +0100

    WIP for open fl testing

--- src/MainOpenFL.hx
+++ src/MainOpenFL.hx
@@ -1,14 +1,17 @@
-import components.SampleView;
+import openfl.display.Sprite;
+import openfl.display.Stage;
 
-class MainHeaps extends hxd.App {
-    override function init() {
-        var tf = new h2d.Text(hxd.res.DefaultFont.get(), s2d);
-        tf.text = "Hello World!";
-        var view = new SampleView(h2d.Tile.fromColor(0xFF,32,32),s2d);
-        // FIXME:
-//        view.mybmp.alpha = 0.8;
+
+class MainOpenFL extends Sprite {
+
+    public function new () {
+        super();
     }
-    static function main() {
-        new MainHeaps();
+
+
+    static function main () {
+        var stage = new Stage (550, 400, 0xFFFFFF, MainOpenFL);
+        js.Browser.document.body.appendChild(stage.element);
     }
+
 }
